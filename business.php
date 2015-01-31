d0e83667ae1830c9b575eac0033dade7d6c6a95e
Brian Schieltz <bschieltz@gmail.com>
2015-01-31 13:57:30 -0500

    Setting up some pages

---
+++ business.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Brian
+ * Date: 1/31/2015
+ * Time: 1:55 PM
+ * Landing page for all users.  If not logged in, show login info.  If logged in, it should be a dashboard of sorts
+ */
---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Brian
+ * Date: 1/31/2015
+ * Time: 1:53 PM
+ */
