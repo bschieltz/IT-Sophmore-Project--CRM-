989720aed58dc9d0de9c403f148c92b2df75a85d
Brian Schieltz <bschieltz@gmail.com>
2015-01-31 23:01:28 -0500

    Setting up some pages

---
+++ employee.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Brian
+ * Date: 1/31/2015
+ * Time: 10:58 PM
+ * Shows Individual Employees
+ */
---
+++ employeemanagement.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Brian
+ * Date: 1/31/2015
+ * Time: 10:58 PM
+ */
