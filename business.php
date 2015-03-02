9ad03935da7667438057f50f3b97a6676cc8a499
Brian Schieltz <bschieltz@gmail.com>
2015-03-01 23:37:15 -0500

    added address to business page.

--- business.php
+++ business.php
@@ -20,21 +20,43 @@ ini_set('display_errors',1);  error_reporting(E_ALL);
     $business = [];
     $employees = [];
     $ucStaff = [];
+    $street1 = "";
+    $street2 = "";
+    $zip_code = "";
+    $city = "";
+    $statePrefix = "";
+
 
     if (!empty($_GET['BusinessID'])) {
         $businessID = $_GET['BusinessID'];
         $businessQuery = "SELECT BusinessName, PrimaryContact, `PrimaryPhone#`, Notes
                   FROM tbusiness WHERE BusinessID = $businessID";
-        $business = mysqli_query($dbc, $businessQuery) or die("Error: ".mysqli_error($dbc));
-        $row = mysqli_fetch_array($business);
-        $businessName = $row['BusinessName'];
-        $primaryContact = $row['PrimaryContact'];
-        $primaryPhoneNumber = $row['PrimaryPhone#'];
-        $notes = $row['Notes'];
+        if ($business = mysqli_query($dbc, $businessQuery)) {
+            $row = mysqli_fetch_array($business);
+            $businessName = $row['BusinessName'];
+            $primaryContact = $row['PrimaryContact'];
+            $primaryPhoneNumber = $row['PrimaryPhone#'];
+            $notes = $row['Notes'];
+        }
+
+        $addressQuery = "SELECT Street1, Street2, zip_code, city, StatePrefix
+            FROM taddress INNER JOIN tzips on taddress.ZipsID = tzips.ZipsID
+                          INNER JOIN tstate on tzips.StateID = tstate.StateID
+            WHERE taddress.BusinessID = $businessID
+            AND taddress.EmployeeID IS NULL";
+        if ($address = mysqli_query($dbc, $addressQuery)) {
+            $row = mysqli_fetch_array($address);
+            $street1 = $row['Street1'];
+            $street2 = $row['Street2'];
+            $zip_code = $row['zip_code'];
+            $city = $row['city'];
+            $statePrefix = $row['StatePrefix'];
+        }
 
         $employeesQuery = "SELECT EmployeeID, FirstName, LastName
             FROM temployee WHERE BusinessID = $businessID and Active = 1";
         $employees = mysqli_query($dbc, $employeesQuery);
+
         $ucStaffQuery = "SELECT tnote.UserID, COUNT(*) as userCount, tuser.FirstName, tuser.LastName
             FROM tnote INNER JOIN tuser ON tnote.UserID = tuser.UserID
             WHERE BusinessID = $businessID
@@ -42,6 +64,7 @@ ini_set('display_errors',1);  error_reporting(E_ALL);
             ORDER BY userCount Desc
             LIMIT 5";
         $ucStaff = mysqli_query($dbc, $ucStaffQuery);
+
     } elseif (!empty($_GET['CreateBusiness'])) {
 
     } elseif (!empty($_GET['Search'])) {
@@ -66,6 +89,8 @@ ini_set('display_errors',1);  error_reporting(E_ALL);
             <li>Business Name: <?= $businessName ?></li>
             <li>Primary Contact: <?= $primaryContact ?></li>
             <li>Phone Number: <?= $primaryPhoneNumber ?></li>
+            <li>Address: <?= $street1 . " " . $street2 ?></li>
+            <li><?= $city . ", " . $statePrefix . " " . $zip_code ?></li>
             <li>Notes: <?= $notes ?></li>
         </ul>
         <input id="editButton" type="submit" value="Edit" />
@@ -89,7 +114,7 @@ ini_set('display_errors',1);  error_reporting(E_ALL);
             if($ucStaff){
                 for($i=0; $i <= mysqli_num_rows($ucStaff); $i++) {
                     if($row = mysqli_fetch_array($ucStaff)) {
-                        print '<dd><a href="user.php?employeeID='. $row['UserID'] . '">' . $row['FirstName'] . " " . $row['LastName'] . '</a></dd>';
+                        print '<dd><a href="user.php?UserID='. $row['UserID'] . '">' . $row['FirstName'] . " " . $row['LastName'] . '</a></dd>';
                     }
                 }
             }
