c8f80d7051609e21064220ccaf76170ca7f1537a
FraxTech <frankalexander14@hotmail.com>
2015-03-15 22:52:08 -0400

    Working on Dashboard Recent Contacts

--- functions.php
+++ functions.php
@@ -292,10 +292,11 @@
                             <ul>
                                 <li>
                                     <div style='color: #E00122'>
-                                        <form class='expandForm'><b><input class='expandButton$i' type='text' value='Note $i' /></b></form>
+                                        <form class='expandForm'><b><input class='expandButton$i' type='submit' value='Note $i' /></b>
                                     </div>
                                         <b>Business: </b><a href='business.php?BusinessID=". $row['BusinessID'] . "'>" . $row['BusinessName'] . "</a>
                                         <b>Date:</b> " . $datetime . "
+                                        </form>
                                 </li>
                                 <div class=DashNote$i>
                                     <ul>
