4f9c47671e5840b83e0ee231c0b559addae7645b
FraxTech <frankalexander14@hotmail.com>
2015-03-18 00:02:29 -0400

    Working on Dashboard Action Items

--- scripts/main.js
+++ scripts/main.js
@@ -61,7 +61,7 @@ $(function() {
 $(function(){
     $('.AIClass').click(function() {
         var clickedID=$(this).attr('id');
-        var toExpand=$(clickedID).next('div[.DashAI]');
+        var toExpand=$(clickedID + 'div[.DashAI]');
         alert(toExpand);
     });
 });
