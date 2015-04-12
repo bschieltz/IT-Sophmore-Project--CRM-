9fb0e7233730f6ac97c914dba05797aef56c200f
FraxTech <frankalexander14@hotmail.com>
2015-04-11 23:41:07 -0400

    Link corrections on index.php

--- functions.php
+++ functions.php
@@ -579,12 +579,12 @@
 			noAuth();
 		}
 
-        $addBusinessLink = "CreateBusiness=True";
-		
 		print "<h2 style='color: #E00122;'>Welcome, $userFullName!</h2>";
 
         print "<br /><form action='notes.php' method='get'><input type='submit' value='Add New Interaction'  class='myButton'/></form>";
-        print "<form action='business.php" . urlencode(htmlspecialchars($addBusinessLink), ENT_QUOTES) ."'><input type='submit' value='Add New Business'  class='myButton'/></form><br />";
+        print "<form action=business.php'>
+               <input type='hidden' name='CreateBusiness' value='True'/>
+               <input type='submit' value='Add New Business'  class='myButton'/></form><br />";
 
 		print "<br /><br />";
         $actionItems = new Interactions();
