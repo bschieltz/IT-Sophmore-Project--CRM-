c783965af4f1f650b2fb10721db6f25ebc3f869b
FraxTech <frankalexander14@hotmail.com>
2015-04-15 15:08:36 -0400

    Working on header.html.

--- logout.php
+++ logout.php
@@ -1,10 +1,11 @@
 <?php
+    require_once('functions.php');
     session_start();
     logout();
     $_SESSION = array();
 
-	define('TITLE', 'UCC CRMS');
-	include('templates/header.html');
+    define('TITLE', 'UCC CRMS');
+    include('templates/header.html');
 ?>
 	<h3 style="color:red">You have been successfully logged out</h3>
 	<p>Please close your browser or log in below</p>
