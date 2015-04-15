e7097ed2526f75f7fbd957dd8f394c07690e715a
FraxTech <frankalexander14@hotmail.com>
2015-04-15 14:48:36 -0400

    Working on header.html.

--- logout.php
+++ logout.php
@@ -2,7 +2,7 @@
 	define('TITLE', 'UCC CRMS');
 	include('templates/header.html');
 	logout();
-    location.reload(true);
+    location.reload();
 ?>
 	<h3 style="color:red">You have been successfully logged out</h3>
 	<p>Please close your browser or log in below</p>
